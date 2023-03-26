5fc1b99ff136041a45b78bf3969802a64b1ced28
Tom Voet <tomvoet@gmx.de>
2023-03-26 22:51:47 +0200

    init

---
+++ app.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import init, { greet } from "./wasm/pkg/wasm"
+
+const test = async () => {
+
+  await init()
+
+  greet("Tom")
+}
+</script>
+
+<template>
+  <div>
+    <button @click="test">test</button>
+  </div>
+</template>
