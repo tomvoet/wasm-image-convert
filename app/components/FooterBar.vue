1e6a65243c7c52f244160d3edefa005737cf5d18
Tom Voet <tomvoet@gmx.de>
2024-10-07 22:44:01 +0200

    fix: update broken repo link

--- app/components/FooterBar.vue
+++ app/components/FooterBar.vue
@@ -1,10 +1,10 @@
 <template>
   <footer class="self-center mt-auto pb-1.5">
     <p class="text-sm text-gray-500 dark:text-gray-400 dark:hover:text-gray-200 hover:text-gray-700">
-      <ULink href="https://github.com/tomvoet/wasm-image-convert" target="_blank" class="flex flex-col items-center">
+      <NuxtLink href="https://github.com/tomvoet/wasm-image-convert" target="_blank" class="flex flex-col items-center">
         You can find the source code on
         <UIcon name="simple-icons:github" class="w-6 h-6 mt-1" />
-      </ULink>
+      </NuxtLink>
     </p>
   </footer>
 </template>
