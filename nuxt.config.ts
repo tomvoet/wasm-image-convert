c713cab42cd364bcf12523f32db24a66f9da9191
Tom Voet <tomvoet@gmx.de>
2024-10-07 22:39:41 +0200

    chore: config pwa settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@vueuse/nuxt', '@nuxt/eslint', '@nuxt/ui'],
+  modules: ['@vueuse/nuxt', '@nuxt/eslint', '@nuxt/ui', '@vite-pwa/nuxt'],
+
+  devtools: { enabled: true },
 
   app: {
     baseURL: '/wasm-image-convert/',
@@ -24,7 +26,56 @@ export default defineNuxtConfig({
 
   icon: {
     clientBundle: {
-      icons: ['simple-icons:github', 'heroicons:moon-20-solid', 'heroicons:sun-20-solid'],
+      icons: ['simple-icons:github', 'heroicons:moon-20-solid', 'heroicons:sun-20-solid', 'heroicons:eye-slash-20-solid', 'heroicons:eye-20-solid', 'heroicons:arrow-path'],
+    },
+  },
+
+  pwa: {
+    base: '/wasm-image-convert/',
+    scope: '/wasm-image-convert/',
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      type: 'module',
+    },
+    client: {
+      installPrompt: true,
+      periodicSyncForUpdates: 60 * 60,
+    },
+    manifest: {
+      name: 'Wasm Image Convert',
+      short_name: 'WIC',
+      description: 'Convert images using WebAssembly',
+      theme_color: '#db2777',
+      lang: 'en',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+      ],
+    },
+    registerType: 'autoUpdate',
+    workbox: {
+      navigateFallback: undefined,
     },
   },
 })
